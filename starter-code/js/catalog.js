52bc7122c489a21242ab5689ab4a1462d3dba9cc
Abinash Shrestha <75637766+abshrestha312@users.noreply.github.com>
2021-07-29 18:07:42 -0500

    DriverAbinash NavigatorSarah

--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -11,9 +11,13 @@ const cart = new Cart([]);
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  //DONE: Add an <option> tag inside the form's select for each product
   const selectElement = document.getElementById('items');
   for (let i in Product.allProducts) {
+    let optionElem = document.createElement('option');
+    optionElem.value = Product.allProducts[i].name;
+    optionElem.textContent = Product.allProducts[i].name;
+    selectElement.appendChild(optionElem);
 
   }
 
